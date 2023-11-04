bbffb9f681a562332987ca9958ab95873bdab5c7
Roberto Santalla <roberto@santalla.io>
2023-11-04 12:47:04 +0100

    off: add a timeout to request, check output looks sane

--- off/openfoodfacts.go
+++ off/openfoodfacts.go
@@ -1,10 +1,13 @@
 package off
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"strings"
+	"time"
 
 	"roob.re/grocy-barcode/provider"
 )
@@ -17,7 +20,15 @@ type product struct {
 type OpenFoodFacts struct{}
 
 func (OpenFoodFacts) Product(barcode string) (provider.Product, error) {
-	resp, err := http.Get(fmt.Sprintf("https://world.openfoodfacts.org/api/v0/product/%s.json", barcode))
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	r, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("https://world.openfoodfacts.org/api/v0/product/%s.json", barcode), nil)
+	if err != nil {
+		return provider.Product{}, fmt.Errorf("building request: %w", err)
+	}
+
+	resp, err := http.DefaultClient.Do(r)
 	if err != nil {
 		return provider.Product{}, fmt.Errorf("making request to off: %w", err)
 	}
@@ -37,6 +48,10 @@ func (OpenFoodFacts) Product(barcode string) (provider.Product, error) {
 	err = json.NewDecoder(resp.Body).Decode(&out)
 	if err != nil {
 		return provider.Product{}, fmt.Errorf("reading product json: %w", err)
+	}
+
+	if out.Product.Name == "" {
+		return provider.Product{}, errors.New("got empty response from off")
 	}
 
 	return provider.Product{
